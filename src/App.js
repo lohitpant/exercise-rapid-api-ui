fd9b53e5e1f27ecdf2eb94926fb5eb3802f55f56
Lohit <lohitpant@gmail.com>
2023-12-27 18:58:57 +0530

    Added Nunito Google Font to MUI theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, ThemeProvider, createTheme } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Footer from './components/Footer';
 
+const googleFont = "'Nunito', sans-serif";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: googleFont
+    }
+})
+
 const App = () => {
     return (
-        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/exercise/:id" element={<ExerciseDetail />} />
-            </Routes>
-            <Footer/>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto" >
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/exercise/:id" element={<ExerciseDetail />} />
+                </Routes>
+                <Footer />
+            </Box>
+        </ThemeProvider>
     )
 }
 
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,17 @@
-import { Stack } from '@mui/material'
+import { Box, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const SearchExercises = () => {
   return (
-    <Stack alignItems='center' mt='37px'>
-
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      <Typography fontWeight={700} sx={{
+        fontSize:{lg:'44px', xs:'30px'} 
+      }} mb="50px" textAlign='center'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField height='76px' value="" onChange={(e)=>{}} placeholder='Search Exercises' type='text' />
+      </Box>
     </Stack>
   )
 }
