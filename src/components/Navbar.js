0a8abf4d0ae87e48572a4567b1a4f055b687ff1d
Lohit <lohitpant@gmail.com>
2024-04-08 00:24:01 +0530

    Updated Exercise Component & code formatting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import Logo from '../assets/images/Logo.png'
-import { Stack } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Logo from "../assets/images/Logo.png";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <Stack className='navbar-container' direction='row' justifyContent='space-around' sx={{gap:{
-            sm:'122px', xs:'40px'
-        }, mt:{sm:'20px', xs:'10px'}, justifyContent:'none'}} px='20px'>
-            <Link to='/'>
-                <img src={Logo} alt='Logo' style={{ width: '48px', height: '48px', margin: '0 20px' }} />
-            </Link>
-            <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
-                <Link to='/' style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>
-                    Home
-                </Link>
-                <a href='#exercises' style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
-            </Stack>
-        </Stack>
-    )
+  return (
+    <Stack
+      className="navbar-container"
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+      sx={{
+        gap: {
+          sm: "122px",
+          xs: "40px",
+        },
+        mt: { sm: "15px", xs: "10px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{ width: "50px", height: "50px", margin: "0 30px" }}
+        />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="22px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderTop: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 }
 
 export default Navbar;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,72 +1,102 @@
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { fetchData, exerciseOptions } from '../utils/fetchData'
-import HorizontalScrollbar from './HorizontalScrollbar';
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { fetchData, exerciseOptions } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const EXERCISE_URL = 'https://exercisedb.p.rapidapi.com/exercises?limit=70';
-const BODY_PARTS_URL = 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList';
+const EXERCISE_URL = "https://exercisedb.p.rapidapi.com/exercises?limit=70";
+const BODY_PARTS_URL =
+  "https://exercisedb.p.rapidapi.com/exercises/bodyPartList";
 
-const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
-
-  const [search, setSearch] = useState('')
-  const [bodyParts, setBodyParts] = useState([])
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExerciseData = async () => {
-      const bodyPartsData = await fetchData(BODY_PARTS_URL, exerciseOptions)
-      setBodyParts(['all', ...bodyPartsData])
+      const bodyPartsData = await fetchData(BODY_PARTS_URL, exerciseOptions);
+      setBodyParts(["all", ...bodyPartsData]);
       console.log(...bodyPartsData);
-    }
-    fetchExerciseData()
-  }, [])
+    };
+    fetchExerciseData();
+  }, []);
 
-  const handleSearch = async () => {
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    console.log(search);
     if (search) {
       const exercisesData = await fetchData(EXERCISE_URL, exerciseOptions);
       console.log(exercisesData);
-      const searchedExercises = exercisesData.filter((exercise) =>
-        exercise.name.toLowerCase().includes(search)
-        || exercise.target.toLowerCase().includes(search)
-        || exercise.equipment.toLowerCase().includes(search)
-        || exercise.bodyPart.toLowerCase().includes(search)
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
       );
-      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-      console.log(search);
+      window.scrollTo({ top: 1000, left: 100, behavior: "smooth" });
       console.log(searchedExercises);
-      setSearch('')
-      setExercises(searchedExercises)
+      setSearch("");
+      setExercises(searchedExercises);
     }
-  }
+  };
 
   return (
-    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
-      <Typography fontWeight={700} sx={{
-        fontSize: { lg: '44px', xs: '30px' }
-      }} mb="50px" textAlign='center'>
+    <Stack alignItems="center" mt="20px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "40px", xs: "28px" },
+        }}
+        mb="30px"
+        textAlign="center"
+      >
         Awesome Exercises You <br /> Should Know
       </Typography>
-      <Box position='relative' mb='72px'>
-        <TextField sx={{
-          input: { fontWeight: '600', fontSize: '18px' },
-          width: { lg: '800px', xs: '350px' },
-          backgroundColor: 'fff', borderRadius: '40px'
-        }} height='76px' value={search} onChange={(e) => { setSearch(e.target.value.toLowerCase()) }}
-          placeholder='Search Exercises' type='text' />
-        <Button className='search-btn' onClick={handleSearch}
-          sx={{
-            bgcolor: '#FF2625',
-            color: '#fff',
-            textTransform: 'none',
-            width: { lg: '175px', xs: '80px' },
-            fontSize: { lg: '20px', xs: '14px' },
-            height: '56px', position: 'absolute', right: 1, top: 1
-          }}>Search</Button>
+      <Box position="relative" mb="72px">
+        <form onSubmit={handleSearch}>
+          <TextField
+            sx={{
+              input: { fontWeight: "600", fontSize: "18px" },
+              width: { lg: "800px", xs: "350px" },
+              backgroundColor: "fff",
+              borderRadius: "40px",
+            }}
+            height="76px"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value.toLowerCase());
+            }}
+            placeholder="Search Exercises"
+            type="text"
+          />
+          <Button
+            className="search-btn"
+            type="submit"
+            sx={{
+              bgcolor: "#FF2625",
+              color: "#fff",
+              textTransform: "none",
+              width: { lg: "175px", xs: "80px" },
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+              position: "absolute",
+              right: 1,
+              top: 1,
+            }}
+          >
+            Search
+          </Button>
+        </form>
       </Box>
-      <Box sx={{ position:'relative', width:'100%', p:'20px'}}>
-          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
